3f685f35915f176cedad2ea536fc838fdc6c6050
Rakib Raza <rakibraza159@gmail.com>
2021-06-12 19:40:18 +0600

    swiper slider install

---
+++ src/components/Review/Review.js
@@ -0,0 +1,15 @@
+import { Container } from '@material-ui/core'
+import { Box } from '@material-ui/core'
+import React from 'react'
+
+const Review = () => {
+  return (
+    <Box py={10}>
+      <Container>
+
+      </Container>
+    </Box>
+  )
+}
+
+export default Review
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,14 @@ import Services from '../components/Services/Services'
 import Blog from '../components/Blog/Blog'
 import Footer from '../components/Footer/Footer'
 import NewsLetter from '../components/NewsLetter/NewsLetter'
+import Review from '../components/Review/Review'
 
 const Home = () => {
   return (
     <>
       <Header />
       <Services />
+      <Review />
       <NewsLetter />
       <Blog />
       <Footer />
